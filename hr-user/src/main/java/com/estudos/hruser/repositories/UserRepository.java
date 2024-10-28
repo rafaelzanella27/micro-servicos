d88a37991b6c2e68e0146e3545291c42e1e6047f
unknown <rafaelzanella@gmail.com>
2024-10-28 10:07:54 -0300

    UserRepository, UserRepository, Zuul Config

---
+++ hr-user/src/main/java/com/estudos/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.estudos.hruser.repositories;
+
+import com.estudos.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
