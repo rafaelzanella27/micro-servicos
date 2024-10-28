ac4e33fc6dc47cc3f346829a3f0b34e5d675e1f6
unknown <rafaelzanella@gmail.com>
2024-10-28 11:01:08 -0300

    User feign client

---
+++ hr-oauth/src/main/java/com/estudos/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package com.estudos.hroauth.services;
+
+import com.estudos.hroauth.entities.User;
+import com.estudos.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email){
+        User user = userFeignClient.findByEmail(email).getBody();
+        if (user == null) {
+            logger.error("Email not found: " + email);
+            throw new IllegalArgumentException("Email not found");
+        }
+        logger.info("Email found: " + email);
+        return user;
+    }
+}
