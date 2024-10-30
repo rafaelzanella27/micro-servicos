d715ae270d17a84dd654eab126ba034124ed6de4
unknown <rafaelzanella@gmail.com>
2024-10-30 10:32:03 -0300

    hr-eureka-server container

--- hr-eureka-server/src/test/java/com/estudos/hreurekaserver/HrEurekaServerApplicationTests.java
+++ hr-eureka-server/src/test/java/com/estudos/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -3,7 +3,7 @@ package com.estudos.hreurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = { HrEurekaServerApplicationTests.class })
 class HrEurekaServerApplicationTests {
 
 	@Test
